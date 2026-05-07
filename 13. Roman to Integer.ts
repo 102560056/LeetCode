814954213de32b2b3403028a85eeab65872d09d1
102560056 <102560056@student.swin.edu.au>
2026-05-07 17:29:01 +1000

    Setup

---
+++ 13. Roman to Integer.ts
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/roman-to-integer
+// Author : Samual Banfield
+// Date   : 07/05/2026
+
+/********************************************************************************** 
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+Given a roman numeral, convert it to an integer.
+**********************************************************************************/
+
+//Setup
+const s :string = "III";
+
+//Task
+function romanToInt(s: string): number {
+    return 3;
+};
+
+//Handle Results
+console.log(romanToInt(s));
